c8c639491b68c05bdb8ce1eea8ed8eb6a2622025
@wage.juli <wage.muhammad13@gmail.com>
2023-07-03 20:19:55 +0700

    create onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,76 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Onboarding',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First App'),
-          backgroundColor: Colors.yellow[700],
+      home: OnboardingPage(),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color primaryColor = const Color(0xFF64B5F6);
+    return Scaffold(
+      body: Center(
+        child: Wrap(
+          direction: Axis.vertical,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 20,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.account_box),
+                Text("WELCOME"),
+              ],
+            ),
+            const Image(
+              width: 400.0,
+              image: AssetImage("images/onboarding-1.png"),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(200.0, 50.0),
+                  foregroundColor: primaryColor,
+                  side: BorderSide(width: 1.0, color: primaryColor)),
+              child: const Text("Sign In"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  minimumSize: const Size(200.0, 50.0),
+                  shadowColor: const Color(0xFFFFFFFF)),
+              child: const Text("Register"),
+            )
+          ],
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/illustration_1.png'),
-            height: 400,
-          ),
+      ),
+    );
+  }
+}
+
+// TODO: will use secondpage when needed
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App'),
+        backgroundColor: Colors.yellow[700],
+      ),
+      body: const Center(
+        child: Image(
+          image: AssetImage('images/illustration_1.png'),
+          height: 400,
         ),
       ),
     );
