9022fa1dba3aeabfe25137562f5be8c9573c4bde
wagejs <wage.muhammad13@gmail.com>
2024-05-12 14:47:07 +0700

    update

--- lib/book_page.dart
+++ lib/book_page.dart
@@ -9,16 +9,15 @@ class BookPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(bookTitle),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        foregroundColor: Colors.black,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-        child: SingleChildScrollView(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(bookTitle),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          foregroundColor: Colors.black,
+        ),
+        body: SizedBox(
+          width: double.infinity,
           child: Column(
             children: [
               Container(
@@ -29,34 +28,34 @@ class BookPage extends StatelessWidget {
                     image: AssetImage(bookImage),
                     fit: BoxFit.cover,
                   ),
-                  borderRadius: BorderRadius.circular(16.0),
                 ),
               ),
               const SizedBox(height: 12.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    bookTitle,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      bookTitle,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 12.0),
-                  const Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black,
+                    const SizedBox(height: 12.0),
+                    const Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
